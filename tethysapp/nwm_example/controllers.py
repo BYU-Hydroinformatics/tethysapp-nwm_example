964a7a08dbf03489ae2a3b26ec136b5ab1577402
drew <zyli2004@gmail.com>
2017-07-21 11:21:47 -0600

    step 6 7 8

--- tethysapp/nwm_example/controllers.py
+++ tethysapp/nwm_example/controllers.py
@@ -2,73 +2,38 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from tethys_sdk.gizmos import Button
 
+import datetime as dt
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import reverse
+from tethys_sdk.gizmos import MapView, Button, MVLayer, MVLegendClass
+import plotly.graph_objs as go
+from tethys_sdk.gizmos import PlotlyView
+# from .helpers import get_nwm_forecast
+
 @login_required()
 def home(request):
     """
     Controller for the app home page.
     """
-    save_button = Button(
-        display_text='',
-        name='save-button',
-        icon='glyphicon glyphicon-floppy-disk',
-        style='success',
-        attributes={
-            'data-toggle':'tooltip',
-            'data-placement':'top',
-            'title':'Save'
-        }
+    # Tiled ArcGIS REST Layer
+    arc_gis_layer2 = MVLayer(
+        source='TileArcGISRest',
+        options={'url': 'https://services.nationalmap.gov/arcgis/rest/services/wbd/MapServer'},
+        legend_title='Watershed Boundaries',
+        legend_extent=[-173, 17, -65, 72]
     )
 
-    edit_button = Button(
-        display_text='',
-        name='edit-button',
-        icon='glyphicon glyphicon-edit',
-        style='warning',
-        attributes={
-            'data-toggle':'tooltip',
-            'data-placement':'top',
-            'title':'Edit'
-        }
-    )
-
-    remove_button = Button(
-        display_text='',
-        name='remove-button',
-        icon='glyphicon glyphicon-remove',
-        style='danger',
-        attributes={
-            'data-toggle':'tooltip',
-            'data-placement':'top',
-            'title':'Remove'
-        }
-    )
-
-    previous_button = Button(
-        display_text='Previous',
-        name='previous-button',
-        attributes={
-            'data-toggle':'tooltip',
-            'data-placement':'top',
-            'title':'Previous'
-        }
-    )
-
-    next_button = Button(
-        display_text='Next',
-        name='next-button',
-        attributes={
-            'data-toggle':'tooltip',
-            'data-placement':'top',
-            'title':'Next'
-        }
+    nwm_example_map = MapView(
+        height='100%',
+        width='100%',
+        layers=[arc_gis_layer2],
+        basemap='OpenStreetMap',
+        legend = True
     )
 
     context = {
-        'save_button': save_button,
-        'edit_button': edit_button,
-        'remove_button': remove_button,
-        'previous_button': previous_button,
-        'next_button': next_button
+        'nwm_example_map': nwm_example_map
     }
 
     return render(request, 'nwm_example/home.html', context)
