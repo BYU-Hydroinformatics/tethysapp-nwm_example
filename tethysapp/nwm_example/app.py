260cdbd9211bd8a9588fa1f2da12b1d927f2ad1d
drew <zyli2004@gmail.com>
2017-07-21 10:51:12 -0600

    first step 4 5

---
+++ tethysapp/nwm_example/app.py
@@ -0,0 +1,34 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class NwmExample(TethysAppBase):
+    """
+    Tethys app class for Nwm Example.
+    """
+
+    name = 'Nwm Example'
+    index = 'nwm_example:home'
+    icon = 'nwm_example/images/icon.gif'
+    package = 'nwm_example'
+    root_url = 'nwm-example'
+    color = '#8e44ad'
+    description = 'Place a brief description of your app here.'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='nwm-example',
+                controller='nwm_example.controllers.home'
+            ),
+        )
+
+        return url_maps
